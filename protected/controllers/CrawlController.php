43d70a09f28099278402ec2a09faf6abe85325ec
Mochamad Gufron <mgufronefendi@gmail.com>
2013-09-13 07:11:29 +0700

    modifying something

--- protected/controllers/CrawlController.php
+++ protected/controllers/CrawlController.php
@@ -98,9 +98,8 @@ class CrawlController extends Controller
 								{
 									$matches[$pattern['pattern_name']] = $filter->nodeValue;
 								}
-								print_r($matches);
 							}
-							if(!empty($matches[$pattern['pattern_name']]))
+							if(!empty($matches) && !empty($matches[$pattern['pattern_name']]))
 							foreach($matches[$pattern['pattern_name']] as $match)
 							{
 								$match = strip_tags($match);
